e2a2a1621bf4eba9ad8e9c25b398bfd66ac75ea7
cgew85 <christoph.gewehr@mailbox.org>
2016-03-10 22:51:06 +0100

    First commit

---
+++ src/main/java/io/github/cgew85/VaadinSpringBootNavigationApplication.java
@@ -0,0 +1,12 @@
+package io.github.cgew85;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinSpringBootNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinSpringBootNavigationApplication.class, args);
+	}
+}
